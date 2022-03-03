e2913c8a367897422d8c4f269eb7146f30fde17d
truc-nguyenminh <truc.nguyenminh93@gmail.com>
2022-03-03 21:30:48 +0700

    call API update & delete

--- frontend/src/pages/NotePage.js
+++ frontend/src/pages/NotePage.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { ReactComponent as ArrowLeft } from '../assets/arrow-left.svg'
 
-const NotePage = ({ match }) => {
+const NotePage = ({ match, history }) => {
     let noteId = match.params.id
     let [note, setNote] = useState(null)
 
@@ -16,16 +16,40 @@ const NotePage = ({ match }) => {
         setNote(data)
     }
 
+    let updateNote = async () => {
+        fetch(`/api/notes/${noteId}/`, {
+            method: "PUT",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(note)
+        })
+    }
+
+    let deleteNote = async () => {
+        fetch(`/api/notes/${noteId}/`, {
+            method: "DELETE",
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        history.push('/')
+    }
+
+    let handleSubmit = () => {
+        updateNote()
+        history.push('/')
+    }
+
     return (
         <div className="note">
             <div className="note-header">
                 <h3>
-                    <Link to="/">
-                        <ArrowLeft />
-                    </Link>
+                    <ArrowLeft onClick={handleSubmit} />
                 </h3>
+                <button onClick={deleteNote}>Delete</button>
             </div>
-            <textarea defaultValue={note?.body}></textarea>
+            <textarea onChange={(e) => { setNote({ ...note, 'body': e.target.value }) }} defaultValue={note?.body}></textarea>
         </div>
     )
 }
